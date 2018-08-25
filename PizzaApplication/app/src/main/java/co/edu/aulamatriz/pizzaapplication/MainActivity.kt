f6cdfc416b5586385c0665cf76d4dc6e6d9ebee5
gustavo <tavomorales88@gmail.com>
2018-08-25 10:50:28 -0500

    upload app pizza

--- PizzaApplication/app/src/main/java/co/edu/aulamatriz/pizzaapplication/MainActivity.kt
+++ PizzaApplication/app/src/main/java/co/edu/aulamatriz/pizzaapplication/MainActivity.kt
@@ -2,11 +2,14 @@ package co.edu.aulamatriz.pizzaapplication
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.content_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,9 +19,38 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
+
+            calcularPrecio()
         }
+    }
+
+    private fun calcularPrecio() {
+
+        var price = 0
+        when {
+            radioGroup.radioButton.isChecked -> price += 7000
+            radioGroup.radioButton2.isChecked -> price += 12000
+            radioGroup.radioButton3.isChecked -> price += 20000
+        }
+
+        if (checkBox.isChecked)
+            price += 2000
+        if (checkBox2.isChecked)
+            price += 1000
+        if (checkBox3.isChecked)
+            price += 3000
+
+        showDialogo(price)
+    }
+
+    private fun showDialogo(price: Int) {
+
+        val dialog = AlertDialog.Builder(this)
+                .setMessage("el valor de la orden es $price")
+                .setPositiveButton("Aceptar", null);
+        dialog.create().show()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
